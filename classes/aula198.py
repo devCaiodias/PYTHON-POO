5344c44d6ee1bd8d1940f10825e24d0eaa60cb5c
Caio Dias <caiodiasmartins12@gmail.com>
2024-03-18 16:36:18 -0300

    class - Classes são moldes para criar novos objetos

---
+++ classes/aula198.py
@@ -0,0 +1,32 @@
+# class - Classes são moldes para criar novos objetos
+# As classes geram novos objetos (instâncias) que
+# podem ter seus próprios atributos e métodos.
+# Os objetos gerados pela classe podem usar seus dados
+# internos para realizar várias ações.
+# Por convenção, usamos PascalCase para nomes de
+# classes.
+
+# string = 'Caio'
+# print(string.upper())
+# print(isinstance(string, str))
+
+class Pessoa:
+    ...
+    
+p1 = Pessoa()
+p1.nome = 'Caio'
+p1.sobrenome = 'Dias'
+
+p2 = Pessoa()
+p2.nome = 'Vini'
+p2.sobrenome = 'Mendez'
+
+print('P1:')
+print(p1.nome)
+print(p1.sobrenome)
+
+print()
+
+print('P2:')
+print(p2.nome)
+print(p2.sobrenome)
