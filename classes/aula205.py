48ead14d4de1b47eccf5b76753e5edf16043e48a
Caio Dias <caiodiasmartins12@gmail.com>
2024-03-19 16:54:03 -0300

    __dict__ e vars para atributos de instância

---
+++ classes/aula205.py
@@ -0,0 +1,24 @@
+# __dict__ e vars para atributos de instância
+class Pessoa:
+    ano_atual = 2024
+    
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        
+    def get_ano_nacimento(self):
+        return Pessoa.ano_atual - self.idade
+    
+dados = {'nome': 'Caio', 'idade': 18}
+p1 = Pessoa(**dados)
+# p1.nome = 'EITA'
+# print(p1.idade)
+# p1.__dict__['outra'] = 'Coisa'
+# p1.__dict__['nome'] = 'EITA'
+# del p1.__dict__['nome']
+# print(p1.__dict__)
+# print(vars(p1))
+# print(p1.outra)
+# print(p1.nome)
+print(vars(p1))
+print(p1.nome)
