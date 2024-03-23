8e2f60063c2b03a2f015aa8c4497688758c49a49
Caio Dias <caiodiasmartins12@gmail.com>
2024-03-23 18:31:26 -0300

    (Parte 2) super e a sobreposição de membros em Python Orientado a Objetos

--- pythonPoo/aula218.py
+++ pythonPoo/aula218.py
@@ -15,26 +15,43 @@
 # string = MinhaString('Caio')
 # print(string.upper())
 
-class A:
+class A(object):
     atributo_a = 'valor = a'
+    
+    def __init__(self, atributo):
+        self.atributo = atributo
+    
     def metado(self):
         print('A')
         
 class B(A):
     atributo_b = 'valor = b'
+    
+    def __init__(self, atributo, outra_coisa):
+        super().__init__(atributo)
+        self.outra_coisa = outra_coisa
+    
     def metado(self):
         print('B')
         
 class C(B):
     atributo_c = 'valor = c'
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print('EI, burlei o sistema')
+    
     def metado(self):
-        super(C, self).metado()
+        # super(B, self).metado()
+        # super(C, self).metado()
+        A.metado(self)
         print('C')
         
 
-c = C()
+c = C('Caio', 'Dias')
 
-print(c.atributo_a)
-print(c.atributo_b)
-print(c.atributo_c)
+print(c.atributo, c.outra_coisa)
+# print(c.atributo_a)
+# print(c.atributo_b)
+# print(c.atributo_c)
 c.metado()
