03f8225358866a5065c6fc6ad7468cdcebf990f5
Caio Dias <caiodiasmartins12@gmail.com>
2024-03-25 17:05:04 -0300

    (Parte 2) Log, LogFileMixin, LogPrintMixin e a união de tudo até aqui

--- pythonPoo/aula220/log.py
+++ pythonPoo/aula220/log.py
@@ -1,15 +1,26 @@
 # Abstração
 class Log:
-    def log(self, msg):
+    def _log(self, msg):
         raise NotImplementedError('Implemente o métado log')
     
+    def log_erro(self,msg):
+        return self._log(f'Erro: {msg}')
+                 
+    def log_sucesso(self,msg):
+        return self._log(f'Sucesso: {msg}')
+    
 class LogFileMixin(Log):
-    def log(self, msg):
+    def _log(self, msg):
         print(msg)
+    
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
     
     
 
 # Testa programa 
-if __name__ == '__main__':    
-    l = LogFileMixin()
-    l.log('Caio')
+if __name__ == '__main__':
+    l = LogPrintMixin()
+    l.log_erro('Vixiii')
+    l.log_sucesso('Caio')
