9ed50ce72a86d5c5b25822911354837d63793ef5
Caio Dias <caiodiasmartins12@gmail.com>
2024-03-25 18:06:43 -0300

    (Parte 4) Eletrônico, Smartphone com Mixin e a união de tudo até aqui

--- pythonPoo/aula220/main.py
+++ pythonPoo/aula220/main.py
@@ -1,1 +1,8 @@
-from log import LogPrintMixin, LogFileMixin
+from eletronico import Smartphone
+
+iplone_12 = Smartphone('Iplone 12')
+iplone_xr = Smartphone('Iplone XR')
+
+
+iplone_12.ligar()
+iplone_12.desligar()
