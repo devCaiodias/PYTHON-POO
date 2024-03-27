9f5bee6d1fbb0d56f2eb8edc121121fbc2a77e37
Caio Dias <caiodiasmartins12@gmail.com>
2024-03-27 15:30:54 -0300

    Python Dunder Methods __repr__ e __str__

--- pythonPoo/aula233.py
+++ pythonPoo/aula233.py
@@ -15,3 +15,24 @@
 # __neg__(self) - -self
 # __str__(self) - str
 # __repr__(self) - str
+
+class Ponto:
+    def __init__(self, x, y, z='String'):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self): # Mais para pegar strig 
+        return f'{self.x}, {self.y}'
+        
+    def __repr__(self): # Mais para desemvolverdorr
+        # class_name = self.__class__.__name__
+        class_name = type(self).__name__
+        return f'{class_name}({self.x!r}, {self.y!r}, {self.z!r})'
+        
+p1 = Ponto(1 ,2)
+p2 = Ponto(15,95)
+
+print(p1)
+print(repr(p2))
+print(f'{p2!r}')
