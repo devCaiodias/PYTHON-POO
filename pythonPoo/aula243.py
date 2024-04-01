872311e6746d74f6929e5e53e1ba6a91858acf0f
Caio Dias <caiodiasmartins12@gmail.com>
2024-04-01 16:11:28 -0300

    Funções decoradoras e decoradores com métodos

--- pythonPoo/aula243.py
+++ pythonPoo/aula243.py
@@ -7,20 +7,17 @@ def meu_repr(self):
         return class_repr
 
 def adicionar_repr(cls):
-    # def meu_repr(self):
-    #     class_name = self.__class__.__name__
-    #     class_dict = self.__dict__
-    #     class_repr = f'{class_name} ({class_dict})'
-    #     return class_repr
     cls.__repr__ = meu_repr
     return cls        
 
-# class ReprMixin:
-#     def __repr__(self):
-#         class_name = self.__class__.__name__
-#         class_dict = self.__dict__
-#         class_repr = f'{class_name} ({class_dict})'
-#         return class_repr
+def meu_planeta(metado):
+    def interno(self, *args, **kwargs):
+        resultado = metado(self, *args, **kwargs)
+    
+        if 'Terra' in resultado:
+            return 'Vc está em casa'
+        return resultado
+    return interno
 
 @adicionar_repr
 class Time:
@@ -32,19 +29,25 @@ class Time:
 class Planeta:
     def __init__(self, nome):
         self.nome = nome
-        
-# Time = adicionar_repr(Time)
+    
+    @meu_planeta
+    def falar_nome(self):
+        return f'O planeta é {self.nome}'
 
 brasil = Time('Brasil')
 portugal = Time('Portugal')
 
-# Planeta = adicionar_repr(Planeta)
 
 terra = Planeta('Terra')
 marte = Planeta('Marte')
+
 
 
 print(brasil)
 print(portugal)
 print(terra)
 print(marte)
+
+
+print(terra.falar_nome())
+print(marte.falar_nome())
