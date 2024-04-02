e6e34b5527918315dff3e0471fc5273312200515
Caio Dias <caiodiasmartins12@gmail.com>
2024-04-02 14:52:02 -0300

    DocStrings de várias linhas (Documentação)

---
+++ pythonPoo/aula250/varias_linhas.py
@@ -0,0 +1,8 @@
+"""O que seu módulo faz
+
+Lorem ipsum dolor sit amet. At iure voluptatem et eius dignissimos est voluptates dolorem qui debitis praesentium? Non officia itaque ab optio minus et porro alias ut fugiat molestiae nam mollitia quidem ad voluptas cupiditate. Aut molestiae fugit aut unde aperiam ut voluptatem rerum hic exercitationem consequuntur et modi cupiditate sit natus cumque et pariatur laborum. Est voluptates ullam ut delectus libero aut vero sint est aliquid culpa et ipsam cumque et cumque natus sed maiores blanditiis.
+
+Eos voluptatem laborum qui voluptatem sapiente est voluptas eligendi sit placeat odio. Ea voluptatum amet id eligendi dolorum ut quae dolor.
+
+Ad voluptate quisquam et unde reiciendis ex rerum dolores. Et asperiores aliquid ut Quis ratione est velit saepe eum nihil perferendis ex iusto adipisci qui laboriosam voluptatibus eum obcaecati animi. Quo optio error non delectus rerum ut recusandae deserunt rem harum quia. Qui facilis veniam sit voluptatem recusandae sit atque maiores.
+"""
