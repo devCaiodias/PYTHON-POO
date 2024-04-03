addcd121dead931ad85494b98bca809bb928ba07
Caio Dias <caiodiasmartins12@gmail.com>
2024-04-03 17:44:33 -0300

    Solução - Criando a classe ContaCorrente

--- execio/ex256/conta.py
+++ execio/ex256/conta.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 class Conta(ABC):
-    def __init__(self, agencia, conta, saldo):
+    def __init__(self, agencia, conta, saldo=0):
         self.agencia = agencia
         self.conta = conta
         self.saldo = saldo
@@ -12,7 +12,7 @@ class Conta(ABC):
         
     def depositar(self, valor):
         self.saldo += valor  
-        self.detalhes(f'Depositar {valor}')  
+        self.detalhes(f'(Depositar {valor})')  
     
     def detalhes(self, msg=''):
         print(f'O seu saldo é {self.saldo:.2f}, {msg}')
@@ -24,13 +24,43 @@ class ContaPoupanca(Conta):
         
         if valor_pos_saque > 0:
             self.saldo -= valor
-            self.detalhes(f'Sacar {valor}')
+            self.detalhes(f'(Sacar {valor})')
         else:
             print('N foi possivel sacar o valor desejado')
-            self.detalhes(f'Saque Negado {valor}')
+            self.detalhes(f'(Saque Negado {valor})')
         
+class ContaCorrente(Conta):
+    def __init__(self, agencia, conta, saldo=0 , limite=0):
+        super().__init__(agencia,conta, saldo)
+        self.limite = limite
+        
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+        limite_maximo = -self.limite
+        
+        if valor_pos_saque >= limite_maximo:
+            self.saldo -= valor
+            self.detalhes(f'(Sacar {valor})')
+            return self.saldo
+        else:
+            print('N foi possivel sacar o valor desejado')
+            print(f'Seu limite é , {-self.limite:.2f}')
+            self.detalhes(f'(Saque Negado {valor})')
+    
         
 if __name__ == '__main__':
-    c_Poupanca = ContaPoupanca(2564,4758,0)
-    c_Poupanca.depositar(20)
-    c_Poupanca.sacar(2)
+    c_Poupanca = ContaPoupanca(2564,4758)
+    c_Poupanca.sacar(1)
+    c_Poupanca.depositar(1)
+    c_Poupanca.sacar(1)
+    c_Poupanca.sacar(1)
+    print('##')
+    c_Corrente =  ContaCorrente(2564,4758, 0, 100)
+    c_Corrente.sacar(1)
+    c_Corrente.depositar(1)
+    c_Corrente.sacar(1)
+    c_Corrente.sacar(1)
+    c_Corrente.sacar(98)
+    c_Corrente.sacar(1)
+    
+    
