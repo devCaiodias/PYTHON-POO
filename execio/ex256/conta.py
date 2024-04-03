238d1b40e4836910da46908fe8378b0c121b86a3
Caio Dias <caiodiasmartins12@gmail.com>
2024-04-03 17:07:53 -0300

    Solução - Criando a classe ContaPoupanca

--- execio/ex256/conta.py
+++ execio/ex256/conta.py
@@ -8,14 +8,29 @@ class Conta(ABC):
     
     @abstractmethod
     def sacar(self, valor):
-        self.saldo - valor
-        self.detalhes('Sacar')
+        pass
         
     def depositar(self, valor):
         self.saldo += valor  
         self.detalhes(f'Depositar {valor}')  
     
     def detalhes(self, msg=''):
-        print(f'O seu saldo é {self.saldo}, {msg}')
+        print(f'O seu saldo é {self.saldo:.2f}, {msg}')
         
         
+class ContaPoupanca(Conta):
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+        
+        if valor_pos_saque > 0:
+            self.saldo -= valor
+            self.detalhes(f'Sacar {valor}')
+        else:
+            print('N foi possivel sacar o valor desejado')
+            self.detalhes(f'Saque Negado {valor}')
+        
+        
+if __name__ == '__main__':
+    c_Poupanca = ContaPoupanca(2564,4758,0)
+    c_Poupanca.depositar(20)
+    c_Poupanca.sacar(2)
