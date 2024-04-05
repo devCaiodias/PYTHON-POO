a8151f7c5eac05d3484524845a6d9332582f099e
Caio Dias <caiodiasmartins12@gmail.com>
2024-04-05 14:07:28 -0300

    Configurações do decorator dataclass

--- pythonPoo/aula265.py
+++ pythonPoo/aula265.py
@@ -7,29 +7,13 @@
 # doc: https://docs.python.org/3/library/dataclasses.html
 from dataclasses import dataclass
 
-@dataclass(init=False)
+@dataclass(repr=True)
 class Pessoa:
     nome: str
     sobrenome: str
     
-    def __init__(self, nome: str, sobrenome: str):
-        self.nome = nome
-        self.sobrenome = sobrenome
-        self.nome_completo = f'{self.nome} {self.sobrenome}'
     
-    # def __post_init__(self):
-    #     self.nome_completo = f'{self.nome} {self.sobrenome}'
-    
-    # @property
-    # def nome_completo(self):
-    #     return f'{self.nome} {self.sobrenome}'
-    
-    # @nome_completo.setter
-    # def nome_completo(self, valor):
-    #     nome, *sobrenome = valor.split()
-    #     self.nome = nome
-    #     self.sobrenome = ' '.join(sobrenome)
     
 if __name__ == '__main__':
     p1 = Pessoa('Caio','Dias')
-    print(p1.nome_completo)
+    print(p1)
