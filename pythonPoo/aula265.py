62e595e67a88b84069846ce07105aa80afb5760f
Caio Dias <caiodiasmartins12@gmail.com>
2024-04-05 13:54:33 -0300

    __init__ e __post_init__ em dataclasses

--- pythonPoo/aula265.py
+++ pythonPoo/aula265.py
@@ -7,23 +7,29 @@
 # doc: https://docs.python.org/3/library/dataclasses.html
 from dataclasses import dataclass
 
-@dataclass
+@dataclass(init=False)
 class Pessoa:
     nome: str
     sobrenome: str
     
-    @property
-    def nome_completo(self):
-        return f'{self.nome} {self.sobrenome}'
-    
-    @nome_completo.setter
-    def nome_completo(self, valor):
-        nome, *sobrenome = valor.split()
+    def __init__(self, nome: str, sobrenome: str):
         self.nome = nome
-        self.sobrenome = ' '.join(sobrenome)
+        self.sobrenome = sobrenome
+        self.nome_completo = f'{self.nome} {self.sobrenome}'
+    
+    # def __post_init__(self):
+    #     self.nome_completo = f'{self.nome} {self.sobrenome}'
+    
+    # @property
+    # def nome_completo(self):
+    #     return f'{self.nome} {self.sobrenome}'
+    
+    # @nome_completo.setter
+    # def nome_completo(self, valor):
+    #     nome, *sobrenome = valor.split()
+    #     self.nome = nome
+    #     self.sobrenome = ' '.join(sobrenome)
     
 if __name__ == '__main__':
     p1 = Pessoa('Caio','Dias')
-    p1.nome_completo = 'Vini Mendez de solza'
-    
     print(p1.nome_completo)
