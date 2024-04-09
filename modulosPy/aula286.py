d42aca31a0d05a6e963d6e50884e1bf5376acb6b
Caio Dias <caiodiasmartins12@gmail.com>
2024-04-09 17:04:53 -0300

    os + shutil - Copiando arquivos e criando pastas com Python

---
+++ modulosPy/aula286.py
@@ -0,0 +1,39 @@
+# os + shutil - Copiando arquivos com Python
+# Vamos copiar arquivos de uma pasta para outra.
+# Copiar -> shutil.copy
+
+
+import os
+import shutil
+# C:\Users\caiod\pythonPOO\modulosPy c:\Users\caiod\exemplopy
+HOME = os.path.expanduser('~')
+DESKTOP = os.path.join(HOME, 'Desktop')
+PASTA_ORIGINAL = os.path.join(DESKTOP, 'exemplopy')
+NOVA_PASTA = os.path.join(DESKTOP, 'NOVA_PASTA')
+print(NOVA_PASTA)
+
+os.makedirs(NOVA_PASTA, exist_ok=True)
+
+for root, dirs, files in os.walk(PASTA_ORIGINAL):
+    for dir_ in dirs:
+        caminho_novo_diretorio = os.path.join(
+            root.replace( PASTA_ORIGINAL, NOVA_PASTA), dir_
+        )
+        # print(caminho_novo_diretorio)
+        os.makedirs(caminho_novo_diretorio, exist_ok=True)
+        
+    for file in files:
+        caminho_file = os.path.join(root, file)
+        caminho_novo = os.path.join(
+            root.replace( PASTA_ORIGINAL, NOVA_PASTA), file
+        )
+        shutil.copy(caminho_file, caminho_novo)
+        
+
+# os + shutil - Apagando, copiando, movendo e renomeando pastas com Python
+# Vamos copiar arquivos de uma pasta para outra.
+# Copiar -> shutil.copy
+# Copiar Árvore recursivamente -> shutil.copytree
+# Apagar Árvore recursivamente -> shutil.rmtree
+# Apagar arquivos -> os.unlink
+# Renomear/Mover -> shutil.move ou os.rename
