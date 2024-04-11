bea08d8ef0ea2081b7e2c97fde73224ea86d92e5
Caio Dias <caiodiasmartins12@gmail.com>
2024-04-11 15:25:21 -0300

    random tem geradores de números pseudoaleatórios (sample, choices, seed)

--- modulosPy/aula295.py
+++ modulosPy/aula295.py
@@ -12,11 +12,12 @@ import random
 # seed
 #   -> Inicializa o gerador de random (por isso "números pseudoaleatórios")
 
+random.seed(0)
 
 # random.randrange(início, fim, passo)
 #   -> Gera um número inteiro aleatório dentro de um intervalo específico
 r_range = random.randrange(0, 10, 2)
-# print(r_range)
+print(r_range)
 
 # random.randint(início, fim)
 #   -> Gera um número inteiro aleatório dentro de um intervalo "sem passo"
@@ -26,17 +27,24 @@ r_range_int = random.randint(2, 20)
 # random.uniform(início, fim)
 #   -> Gera um número flutuante aleatório dentro de um intervalo "sem passo"
 r_range_float = random.uniform(1.2, 10.9)
-print(r_range_float)
+# print(r_range_float)
 
 # random.shuffle(SequenciaMutável) -> Embaralha a lista original
-
+nome = ['Caio', 'Debora', 'Vinicius', 'Joao']
+# random.shuffle(nome)
+# print(nome)
 
 # random.sample(Iterável, k=N)
 #   -> Escolhe elementos do iterável e retorna outro iterável (não repete)
-
+# novos_nomes = random.sample(nome, k=2)
+# print(novos_nomes)
 
 # random.choices(Iterável, k=N)
 #   -> Escolhe elementos do iterável e retorna outro iterável (repete valores)
+novos_nomes = random.choices(nome, k=2)
+# print(nome)
+# print(novos_nomes)
 
 
 # random.choice(Iterável) -> Escolhe um elemento do iterável
+# print(random.choice(nome))
