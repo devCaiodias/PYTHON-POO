ce2de12bceec3fa11b6867cba65e9753a41bb12f
Caio Dias <caiodiasmartins12@gmail.com>
2024-04-11 15:10:32 -0300

    random tem geradores de números pseudoaleatórios (randrange, randint, uniform)

---
+++ modulosPy/aula295.py
@@ -0,0 +1,42 @@
+# random tem geradores de números pseudoaleatórios
+# Obs.: números pseudoaleatórios significa que os números
+# parecem ser aleatórios, mas na verdade não são. Portanto,
+# este módulo não deve ser usado para segurança ou uso criptográfico.
+# O motivo disso é que quando temos uma mesma entrada e um mesmo algorítimo,
+# a saída pode ser previsível.
+# doc: https://docs.python.org/pt-br/3/library/random.html
+import random
+
+
+# Funções:
+# seed
+#   -> Inicializa o gerador de random (por isso "números pseudoaleatórios")
+
+
+# random.randrange(início, fim, passo)
+#   -> Gera um número inteiro aleatório dentro de um intervalo específico
+r_range = random.randrange(0, 10, 2)
+# print(r_range)
+
+# random.randint(início, fim)
+#   -> Gera um número inteiro aleatório dentro de um intervalo "sem passo"
+r_range_int = random.randint(2, 20)
+# print(r_range_int)
+
+# random.uniform(início, fim)
+#   -> Gera um número flutuante aleatório dentro de um intervalo "sem passo"
+r_range_float = random.uniform(1.2, 10.9)
+print(r_range_float)
+
+# random.shuffle(SequenciaMutável) -> Embaralha a lista original
+
+
+# random.sample(Iterável, k=N)
+#   -> Escolhe elementos do iterável e retorna outro iterável (não repete)
+
+
+# random.choices(Iterável, k=N)
+#   -> Escolhe elementos do iterável e retorna outro iterável (repete valores)
+
+
+# random.choice(Iterável) -> Escolhe um elemento do iterável
