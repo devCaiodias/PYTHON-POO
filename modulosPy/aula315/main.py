49ae409fd89d19613af39bf63f99e3a9a14ecae0
Caio Dias <caiodiasmartins12@gmail.com>
2024-04-17 13:25:34 -0300

    Selenium - Selecionando elementos com By, expected_conditions e WebDriverWait

--- modulosPy/aula315/main.py
+++ modulosPy/aula315/main.py
@@ -1,1 +1,83 @@
-print('Olá mundo')
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from pathlib import Path
+
+# RAIZ_PASTA = Path(__file__).parent
+# CHROMEDRIVER_EXEC = RAIZ_PASTA / 'driver' / 'chromedriver.exe'
+
+# chrome_options = webdriver.ChromeOptions()
+# chrome_service = Service(executable_path=CHROMEDRIVER_EXEC) # type: ignore
+# chrome_browser = webdriver.Chrome(
+#     service=chrome_service,
+#     options=chrome_options,
+# )
+
+# chrome_browser.get('https://www.lofi.cafe/')
+# time.sleep(30)
+
+
+
+
+
+# Selenium - Automatizando tarefas no navegador
+from pathlib import Path
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Chrome Options
+# https://peter.sh/experiments/chromium-command-line-switches/
+
+
+# Caminho para a raiz do projeto
+ROOT_FOLDER = Path(__file__).parent
+# Caminho para a pasta onde o chromedriver está
+CHROME_DRIVER_PATH = ROOT_FOLDER / 'driver' / 'chromedriver.exe'
+
+
+def make_chrome_browser(*options: str) -> webdriver.Chrome:
+    chrome_options = webdriver.ChromeOptions()
+
+    # chrome_options.add_argument('--headless')
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)  # type: ignore
+
+    chrome_service = Service(
+        executable_path=str(CHROME_DRIVER_PATH),
+    )
+
+    browser = webdriver.Chrome(
+        service=chrome_service,
+        options=chrome_options
+    )
+
+    return browser
+
+
+if __name__ == '__main__':
+    TIME_TO_WAIT = 10
+    
+    # Example
+    # options = '--headless', '--disable-gpu',
+    options = ()
+    browser = make_chrome_browser(*options)
+
+    # Como antes
+    browser.get('https://www.google.com')
+    
+    # Espere para encontrar o input
+    search_input = WebDriverWait(browser, TIME_TO_WAIT).until(
+        EC.presence_of_element_located(
+            (By.NAME, 'q')
+        )
+    )
+    search_input.send_keys('Caio desenho')
+
+    # Dorme por 10 segundos
+    sleep(TIME_TO_WAIT)
