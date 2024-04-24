b4f67b516d297c97c178717a67d3cea830db2a19
Caio Dias <caiodiasmartins12@gmail.com>
2024-04-24 11:44:20 -0300

    Calculadora: criando um botão com QPushButton no PySide6

--- interface_grafica/aula346/main.py
+++ interface_grafica/aula346/main.py
@@ -1,11 +1,65 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from display import Display
 from PySide6.QtGui import QIcon
 from variables import WINDOW_ICON_PATH
-from info import Infor
+from PySide6.QtWidgets import QLabel
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QLineEdit
+from variables import BIG_FONT_SIZE, MEDIUM_FONT_SIZE, MiNINUM_WIDTH
 
-from main_window import MainWindow
+from buttons import Button
+
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout
+
+
+# # Cria aplicação
+class MainWindow(QMainWindow):
+    def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
+        super().__init__(parent, *args, **kwargs)
+        
+        
+        # Configurando o layout basico
+        self.cw = QWidget()
+        self.vLayout = QVBoxLayout()
+        self.cw.setLayout(self.vLayout)
+        self.setCentralWidget(self.cw)
+        
+        # Titulo da janela
+        self.setWindowTitle('Calculadora')
+        
+    def adjustFixedSize(self):
+        # Ultima coisa a fazer 
+        self.adjustSize()
+        self.setFixedSize(self.width(), self.height())
+    
+    def addWidgetToVLayout(self, widget: QWidget):
+        self.vLayout.addWidget(widget)
+     
+        
+# Infor
+class Infor(QLabel):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.configStyle()
+        
+    def configStyle(self):
+        self.setAlignment(Qt.AlignmentFlag.AlignRight)     
+
+
+
+# Display
+class Display(QLineEdit):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.configStyle()
+        
+    def configStyle(self):
+        self.setStyleSheet(f'font-size: {BIG_FONT_SIZE}px')
+        self.setMinimumHeight(BIG_FONT_SIZE * 2)
+        self.setMinimumWidth(MEDIUM_FONT_SIZE)
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+
 
 if __name__ == '__main__':
     # snake_case
@@ -28,6 +82,10 @@ if __name__ == '__main__':
     display = Display()
     display.setPlaceholderText('Number')
     window.addWidgetToVLayout(display)
+    
+    # Button
+    button = Button('Text button')
+    window.addWidgetToVLayout(button)
     
     
     # Executa tudo
