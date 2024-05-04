b2e7470c4b267648bff305f1512ec0eac71406ac
Caio Dias <caiodiasmartins12@gmail.com>
2024-05-04 17:30:24 -0300

    QObject e QThread: o problema!

--- interface_grafica/aula378/src/mian.py
+++ interface_grafica/aula378/src/mian.py
@@ -1,5 +1,8 @@
+from PySide6.QtCore import QEvent, QObject
 from PySide6.QtWidgets import QMainWindow, QApplication
+from PySide6.QtGui import QKeyEvent
 from window import Ui_MainWindow
+from typing import cast
 import sys
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -9,9 +12,17 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         
         self.pushButton.clicked.connect(self.changeLabelResult)
         
+        self.LineName.installEventFilter(self)
+        
     def changeLabelResult(self):
         text = self.LineName.text()
         self.LabelResult.setText(text)
+        
+    def eventFilter(self, watched: QObject, event: QEvent) -> bool:
+        if event.type() == QEvent.Type.KeyPress: 
+            event = cast(QKeyEvent, event)
+            print(event.text())
+        return super().eventFilter(watched, event)
         
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- interface_grafica/aula381/main.py
+++ interface_grafica/aula381/main.py
@@ -1,11 +1,27 @@
 from PySide6.QtWidgets import QWidget, QApplication
 from ui_workrui import Ui_myWidget
 import sys
+import time
 
 class MyWidget(QWidget, Ui_myWidget):
     def __init__(self, parent= None) -> None:
         super().__init__(parent)
         self.setupUi(self)
+        
+        self.Button1.clicked.connect(self.hardWork1)
+        self.Button2.clicked.connect(self.hardWork2)
+        
+    def hardWork1(self):
+        for i in range(5):
+            print(i)
+            time.sleep(1)
+            self.label1.setText('hardWork1 terminado')
+        
+    def hardWork2(self):
+        for i in range(5):
+            print(i)
+            time.sleep(1)
+        self.label2.setText('hardWork2 terminado')
         
 if __name__ == '__main__':
     app= QApplication(sys.argv)
